2ef3763635619c9c8b8a15078b375c2e750ab56c
Jeff Ruoss <jeff@zenthree.com>
2022-09-19 17:59:24 -0700

    Create player velocity var.

--- js/PlayerMovement.js
+++ js/PlayerMovement.js
@@ -3,45 +3,46 @@ export default class PlayerMovement {
     this.player = player;
     this.keys = keys;
     this.player.direction = 'down';
+    this.player.velecity = 300;
   }
 
   playerMove() {
     this.player.setVelocity(0);
 
     if (this.keys.A.isDown && this.keys.W.isDown) {
-      this.player.setVelocityX(-1000);
-      this.player.setVelocityY(-1000);
+      this.player.setVelocityX(-this.player.velecity);
+      this.player.setVelocityY(-this.player.velecity);
       this.player.direction = 'left';
       this.player.anims.play('Walk left', true);
     } else if (this.keys.A.isDown && this.keys.S.isDown) {
-      this.player.setVelocityX(-1000);
-      this.player.setVelocityY(1000);
+      this.player.setVelocityX(-this.player.velecity);
+      this.player.setVelocityY(this.player.velecity);
       this.player.anims.play('Walk left', true);
       this.player.direction = 'left';
     } else if (this.keys.D.isDown && this.keys.W.isDown) {
-      this.player.setVelocityX(1000);
-      this.player.setVelocityY(-1000);
+      this.player.setVelocityX(this.player.velecity);
+      this.player.setVelocityY(-this.player.velecity);
       this.player.anims.play('Walk right', true);
       this.player.direction = 'right';
     } else if (this.keys.D.isDown && this.keys.S.isDown) {
-      this.player.setVelocityX(1000);
-      this.player.setVelocityY(1000);
+      this.player.setVelocityX(this.player.velecity);
+      this.player.setVelocityY(this.player.velecity);
       this.player.anims.play('Walk right', true);
       this.player.direction = 'right';
     } else if (this.keys.A.isDown) {
-      this.player.setVelocityX(-1000);
+      this.player.setVelocityX(-this.player.velecity);
       this.player.play('Walk left', true);
       this.player.direction = 'left';
     } else if (this.keys.D.isDown) {
-      this.player.setVelocityX(1000);
+      this.player.setVelocityX(this.player.velecity);
       this.player.play('Walk right', true);
       this.player.direction = 'right';
     } else if (this.keys.W.isDown) {
-      this.player.setVelocityY(-1000);
+      this.player.setVelocityY(-this.player.velecity);
       this.player.play('Walk up', true);
       this.player.direction = 'up';
     } else if (this.keys.S.isDown) {
-      this.player.setVelocityY(1000);
+      this.player.setVelocityY(this.player.velecity);
       this.player.play('Walk down', true);
       this.player.direction = 'down';
     } else {
