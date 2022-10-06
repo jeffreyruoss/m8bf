e611deb935f67f76f1f63001b735d2330a5e8629
Jeff Ruoss <jeff@zenthree.com>
2022-10-05 21:23:30 -0700

    Build: size hitbox, collide with player, set tint on preplace image to green.

--- js/BuildManager.js
+++ js/BuildManager.js
@@ -4,15 +4,13 @@ export default class BuildManager {
     this.prePlaceStructure = null;
   }
 
-  build(structure) {
-
+  build(key) {
     // get the position of the mouse
     const pointer = this.scene.input.activePointer;
 
     // pre-placed structure image
-    this.prePlaceStructure = this.scene.physics
-      .add.sprite(pointer.worldX, pointer.worldY, structure)
-      .setSize(32, 40).setAlpha(0.5);
+    this.prePlaceStructure = this.scene.physics.add.image(pointer.worldX, pointer.worldY, key)
+      .setSize(32, 40).setAlpha(0.7).setTint(0xa1ff4b);
 
     this.scene.input.on('pointerdown', (pointer) => {
       if (this.prePlaceStructure) {
@@ -22,10 +20,11 @@ export default class BuildManager {
         this.prePlaceStructure = null;
 
         // place the actual structure
-        this.scene.physics
-          .add.sprite(pointer.worldX, pointer.worldY, structure)
-          .setSize(32, 40);
+        const structure = this.scene.physics.add.staticSprite(pointer.worldX, pointer.worldY, key)
+          .setSize(64, 64);
         this.scene.sound.play('place-structure');
+
+        this.scene.physics.add.collider(this.scene.player, structure);
       }
     });
 
