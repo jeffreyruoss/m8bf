6ff3ea96a4c25c9c9764163f24933268d246416f
Jeff Ruoss <jeff@zenthree.com>
2022-10-08 12:57:33 -0700

    Clean up the menu panels and fix the craft button.

--- js/menu/panels/BuildPanel.js
+++ js/menu/panels/BuildPanel.js
@@ -11,9 +11,9 @@ export default class BuildPanel {
     const margin = 12;
     const width = Menu.box.width - Menu.padding * 2;
     const height = 64 + padding * 2;
-    const itemsJSON = this.scene.itemsJSON
-    for (let item in itemsJSON) {
-      if (!itemsJSON[item].buildable) continue;
+    const items = this.scene.itemsJSON
+    for (let item in items) {
+      if (!items[item].buildable) continue;
       const inInventory = this.inInventory(item);
       const rectangleBackgroundColor = inInventory ? 0x27253b : 0x1d1b2c;
 
@@ -28,16 +28,20 @@ export default class BuildPanel {
       buttonText.y = buttonRectangle.y - buttonText.height / 2 + buttonRectangle.height / 2;
       this.scene.menuItems.add(buttonText);
       this.scene.menuItems.add(buttonRectangle);
-      if (inInventory) {
-        buttonRectangle.setInteractive();
-        buttonRectangle.on("pointerdown", () => {
+      buttonRectangle.setInteractive();
+      buttonRectangle.on("pointerdown", () => {
+        if (inInventory) {
           if (this.scene.Build.prePlaceStructure === null) {
             this.scene.Menu.toggleMenu(this.scene.Menu.currentPanel)
             this.scene.Build.build(item);
             this.scene.player.inventory[item] -= 1;
           }
-        });
-      } else {
+        } else {
+          const messageStyle = { fontSize: "18px", fontFamily: this.scene.font, align: "center", color: "#AE3737", backgroundColor: "#ffffff", padding: 12 };
+          this.scene.MessageManager.createMessage(buttonText.x, buttonText.y, "You don't have this item in your inventory", messageStyle);
+        }
+      });
+      if (!inInventory) {
         buttonRectangle.alpha = 0.2;
         buttonText.alpha = 0.2;
       }
@@ -46,11 +50,11 @@ export default class BuildPanel {
       this.scene.menuItems.add(image);
 
       const titleStyle = { fontSize: "22px", fontFamily: this.scene.font, align: "center" };
-      const titleText = this.scene.add.text(image.x + image.width + padding, y + padding, itemsJSON[item].name, titleStyle);
+      const titleText = this.scene.add.text(image.x + image.width + padding, y + padding, items[item].name, titleStyle);
       this.scene.menuItems.add(titleText);
 
       const descriptionStyle = { fontSize: "18px", fontFamily: this.scene.font, align: "center" };
-      const descriptionText = this.scene.add.text(titleText.x, titleText.y + titleText.height + padding + 5, itemsJSON[item].description, descriptionStyle);
+      const descriptionText = this.scene.add.text(titleText.x, titleText.y + titleText.height + padding + 5, items[item].description, descriptionStyle);
       this.scene.menuItems.add(descriptionText);
 
       y += height + margin;
--- js/menu/panels/CraftPanel.js
+++ js/menu/panels/CraftPanel.js
@@ -5,13 +5,13 @@ export default class CraftPanel {
 
   // This method is called dynamically from Menu.js
   createCraftPanel(Menu) {
-    const items = this.scene.itemsJSON;
     let x = Menu.box.x + Menu.padding;
     let y = Menu.box.y + Menu.padding + Menu.navHeight;
     let spacing = 5;
     let padding = 11;
     let width = Menu.box.width - Menu.padding * 2;
     let height = 64 + padding * 2;
+    const items = this.scene.itemsJSON;
     for (let item in items) {
       const name = items[item].name;
       const description = items[item].description;
@@ -24,22 +24,35 @@ export default class CraftPanel {
       recipe = recipeArray.join(" / ");
       recipe = recipe.replace(/\b\w/g, l => l.toUpperCase());
       const isEnoughResources = this.scene.Craft.isEnoughResources(items[item]);
-      let boxColor = isEnoughResources ? 0x27253b : 0x1d1b2c;
-      this.scene.menuItems.add(this.scene.add.rectangle(x, y, width, height, boxColor));
+      const boxColor = isEnoughResources ? 0x27253b : 0x1d1b2c;
 
-      const style = { fontSize: "20px", fontFamily: this.scene.font, fill: "#ffffff", backgroundColor: "#3f3f74", padding: 24.5, textAlign: "center" };
-      const craftButton = this.scene.add.text(x + padding, y + padding, 'CRAFT', style);
-      this.scene.menuItems.add(craftButton);
-      if (isEnoughResources) {
-        craftButton.setInteractive().on("pointerdown", () => {
-          this.scene.Craft.craftItem(item, items[item]);
-          Menu.updateMenu();
-        });
-      } else {
-        craftButton.alpha = 0.2;
+      const rectangle = this.scene.add.rectangle(x, y, width, height, boxColor);
+      this.scene.menuItems.add(rectangle);
+
+      const buttonStyle = { fontSize: "20px", fontFamily: this.scene.font, align: "center" };
+      const buttonText = this.scene.add.text(x + padding, y + padding, 'Craft', buttonStyle);
+      const buttonRectangle = this.scene.add.rectangle(x + padding, y + padding, buttonText.width + padding * 2 , rectangle.height - padding * 2, 0x3F3F74);
+      buttonText.depth = 1
+      buttonText.x = buttonRectangle.x - buttonText.width / 2 + buttonRectangle.width / 2;
+      buttonText.y = buttonRectangle.y - buttonText.height / 2 + buttonRectangle.height / 2;
+      this.scene.menuItems.add(buttonText);
+      this.scene.menuItems.add(buttonRectangle);
+      buttonRectangle.setInteractive();
+      buttonRectangle.on("pointerdown", () => {
+        if (isEnoughResources) {
+            this.scene.Craft.craftItem(item, items[item]);
+            Menu.updateMenu();
+        } else {
+          const messageStyle = { fontSize: "18px", fontFamily: this.scene.font, align: "center", color: "#AE3737", backgroundColor: "#ffffff", padding: 12 };
+          this.scene.MessageManager.createMessage(buttonText.x, buttonText.y, "You don't have the appropriate ingredients.", messageStyle);
+        }
+      });
+      if (!isEnoughResources) {
+        buttonRectangle.alpha = 0.2;
+        buttonText.alpha = 0.2;
       }
 
-      const itemPlaceholder = this.scene.add.rectangle(craftButton.x + craftButton.width + padding, y + padding, 64, 64, 0x9AACB6);
+      const itemPlaceholder = this.scene.add.rectangle(buttonRectangle.x + buttonRectangle.width + padding, y + padding, 64, 64, 0x9AACB6);
       this.scene.menuItems.add(itemPlaceholder);
 
       const itemTitle = this.scene.add
