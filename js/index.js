8d2d5fe3e232e5bbff541e6a20e7323bd21430c1
Jeff Ruoss <jeff@zenthree.com>
2022-10-10 13:01:37 -0700

    Screensize comments.

--- js/index.js
+++ js/index.js
@@ -3,12 +3,17 @@ import Main from "./scenes/Main.js";
 
 const config = {
   type: Phaser.WEBGL,
-  width: window.innerWidth * window.devicePixelRatio, // Fit to screen
-  height: window.innerHeight * window.devicePixelRatio, // Fit to screen
-  // width: 256 * 4, // NES * 4
-  // height: 240 * 4, // NES * 4
-  // maxWidth: 256 * 4, // NES * 4
-  // maxHeight: 240 * 4, // NES * 4
+
+  // Uncomment the following 2 lines for Fullscreen mode
+  // width: window.innerWidth * window.devicePixelRatio,
+  // height: window.innerHeight * window.devicePixelRatio,
+
+  // Uncomment the following 4 lines for NES screen size mode (256x240 * 4)
+  width: 256 * 4, // NES * 4
+  height: 240 * 4, // NES * 4
+  maxWidth: 256 * 4, // NES * 4
+  maxHeight: 240 * 4, // NES * 4
+
   pixelArt: true,
   // scene: [Intro, Main],
   scene: [Main, Intro],
