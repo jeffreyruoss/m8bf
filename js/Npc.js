85511c6eb1a6917695a3d4dcf61c8b9c8762aef0
Jeff Ruoss <jeff@zenthree.com>
2022-10-29 21:05:30 -0700

    Finish NPC.

--- js/Npc.js
+++ js/Npc.js
@@ -1,7 +1,14 @@
 export default class Npc extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y) {
     super(scene, x, y, 'npc');
+    this.play({ key: 'Idle down - npc', repeat: -1 });
+    this.setDepth(2);
+    scene.physics.add.existing(this);
     scene.add.existing(this);
-    this.play({ key: 'Idle down - npc', repeat: -1 })
+    this.body.setSize(35, 25);
+    this.body.setOffset(17, 28);
+    this.scene.allObjects.add(this);
+    this.scene.physics.add.collider(this, scene.allObjects);
+    this.body.immovable = true;
   }
 }
