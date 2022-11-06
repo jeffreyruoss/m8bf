6189064dc8cc30ae65c1cddd5095d8cc8c57f07c
Jeff Ruoss <jeff@zenthree.com>
2022-11-06 11:02:34 -0700

    Add NPC name to dialog box.

--- js/playerActions/actions/Dialog.js
+++ js/playerActions/actions/Dialog.js
@@ -11,11 +11,12 @@ export default class Dialog {
     if (npc) {
       this.scene.StoryLogic.npcsBeforeDialog(npc);
       const npcKey = npc.data.get('npcKey');
+      const npcName = npc.data.get('npcName');
       const dialogNumber = npc.data.get('dialogNumber');
       const dialogStatus = npc.data.get('dialogStatus');
       const dialog = this.scene.dialogJSON[npcKey].dialog[dialogNumber][dialogStatus];
       if (this.dialogueActive === false) {
-        this.startDialog(dialog);
+        this.startDialog(dialog, npcName);
       } else if (this.dialogueStep + 1 < this.dialogueLength){
         this.continueDialog(dialog);
       } else {
@@ -39,19 +40,26 @@ export default class Dialog {
     return npc;
   }
 
-  startDialog(dialog) {
+  startDialog(dialog, npcName) {
     this.dialogueActive = true;
     this.dialogueLength = Object.keys(dialog).length;
     this.scene.player.enabled = false;
     const width = this.scene.cameras.main.width - 60;
-    const height = 100;
+    const height = 150;
     const x = this.scene.cameras.main.worldView.x + 30;
-    const y = this.scene.cameras.main.worldView.y + this.scene.cameras.main.height - height - 30;
+    const y = this.scene.cameras.main.worldView.y + this.scene.cameras.main.height - height - 80;
     this.dialogBox = this.scene.add.rectangle(x, y, width, height, 0xffffff, 1);
     this.dialogBox.setOrigin(0);
     this.dialogBox.setDepth(5);
-    const style = { fontFamily: this.scene.font, fontSize: 24 , fill: '#000000' };
-    this.dialogText = this.scene.add.text(x + 10, y + 10, dialog[this.dialogueStep], style);
+    let style = { fontFamily: this.scene.font, fontSize: 22 , fill: '#386483' };
+    this.dialogNpcName = this.scene.add.text(x + 15, y + 15, npcName, style);
+    this.dialogNpcName.setDepth(5);
+    this.dialogNpcName.line = this.scene.add.line(x + 15, y + 50, 0, 0, this.dialogNpcName.width + 50, 0, 0x386483, 1);
+    this.dialogNpcName.line.setDepth(5);
+    this.dialogNpcName.line.alpha = 0.3;
+    this.dialogNpcName.line.setOrigin(0);
+    style = { fontFamily: this.scene.font, fontSize: 24 , fill: '#000000' };
+    this.dialogText = this.scene.add.text(x + 15, y + 65, dialog[this.dialogueStep], style);
     this.dialogText.setDepth(5);
   }
 
@@ -65,6 +73,8 @@ export default class Dialog {
     this.dialogueStep = 0;
     this.scene.player.enabled = true;
     this.dialogBox.destroy();
+    this.dialogNpcName.destroy();
+    this.dialogNpcName.line.destroy();
     this.dialogText.destroy();
     if (dialogStatus === 'initial') {
       this.scene.StoryLogic.npcsAfterDialog(npc);
