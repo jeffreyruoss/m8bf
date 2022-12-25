6ecf31ce8b500981157f52af8b0af08cee69d646
Jeff Ruoss <jeff@zenthree.com>
2022-12-25 11:58:32 -0700

    Get started on Automaton menu.

--- js/playerActions/PlayerActions.js
+++ js/playerActions/PlayerActions.js
@@ -2,6 +2,7 @@ import Dialog from './actions/Dialog.js';
 import Collect from './actions/Collect.js';
 import PickUp from './actions/PickUp.js';
 import SmeltMenu from './actions/SmeltMenu.js';
+import AutomatonMenu from './actions/AutomatonMenu.js';
 import Inspect from './actions/Inspect.js';
 
 export default class PlayerActions {
@@ -11,6 +12,7 @@ export default class PlayerActions {
     this.Collect = new Collect(scene);
     this.PickUp = new PickUp(scene);
     this.SmeltMenu = new SmeltMenu(scene);
+    this.AutomatonMenu = new AutomatonMenu(scene);
     this.Inspect = new Inspect(scene);
   }
 
@@ -28,6 +30,10 @@ export default class PlayerActions {
 
   smelt() {
     this.SmeltMenu.init();
+  }
+
+  automaton() {
+    this.AutomatonMenu.init();
   }
 
   inspect() {
